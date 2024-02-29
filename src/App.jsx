35150d7cae9f761b3ba62cf2cc0c7d01376fab07
sergiuchilat <chilatsergiu@gmail.com>
2024-02-29 12:29:40 +0200

    Add localisation (EN/RO)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [language, setLanguage] = useState('RO');
+
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+  }
+
+
   return (
     <div className="App">
-      <h1>React App</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {
+          language === 'EN' && <>
+            This is a React APP
+          </>
+        }
+
+        {
+          language === 'RO' && <>
+            Aceasta este o aplicatie React
+          </>
+        }
+
+      </h1>
+      <p>
+        {
+          language === 'EN' && <>
+            This is a simple React app that is created using create-react-app.
+          </>
+        }
+
+        {
+          language === 'RO' && <>
+            Aceasta este o aplicatie simpla React care este creata folosind create-react-app.
+          </>
+        }
+      </p>
+      <p>
+        <button
+          className={language === 'EN' ? 'selected-language' : ''}
+          disabled={language === 'EN'}
+          onClick={() => changeLanguage('EN')}
+        >EN</button>
+        <button
+          className={language === 'RO' ? 'selected-language' : ''}
+          disabled={language === 'RO'}
+          onClick={() => changeLanguage('RO')}
+        >RO</button>
+      </p>
     </div>
   );
 }
